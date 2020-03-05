f5dd42649d16f863ea7584c0c234a129a827bb93
DeeMeyers <justdmeyers@gmail.com>
2020-03-05 12:30:56 -0500

    still needs work on settings error

--- habitTracker/core/views.py
+++ habitTracker/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your views here.
 def launch_home(request):
--- habitTracker/habitTracker/urls.py
+++ habitTracker/habitTracker/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from core import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path('admin/', admin.site.urls),
